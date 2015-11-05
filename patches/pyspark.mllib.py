ff3bc315c29e18ce2189e15528caf8a17b73ceb5
Izhar Firdaus <kagesenshi.87@gmail.com>
2015-11-05 16:04:56 +0800

    add patch to mllib init for numpy >= 1.10

---
+++ patches/pyspark.mllib.py
@@ -0,0 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Python bindings for MLlib.
+"""
+
+# MLlib currently needs NumPy 1.4+, so complain if lower
+
+import numpy
+
+version = [int(x) for x in numpy.version.version.split('.')]
+if len(version) == 2:
+    version += [0]
+if len(version) != 3:
+    raise Exception('Unknown version format: %s' % numpy.version.version)
+
+if tuple(version) < (1,10,0):
+    raise Exception("MLlib requires NumPy 1.4+")
+
+__all__ = ['classification', 'clustering', 'feature', 'linalg', 'random',
+           'recommendation', 'regression', 'stat', 'tree', 'util']
+
+import sys
+import rand as random
+random.__name__ = 'random'
+random.RandomRDDs.__module__ = __name__ + '.random'
+sys.modules[__name__ + '.random'] = random
+
