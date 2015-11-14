8a133586a59142594a201f2cc36dd5a5133122ff
Izhar Firdaus <kagesenshi.87@gmail.com>
2015-11-14 09:29:46 +0800

    refactor playbook into smaller components

--- config/jupyterhub_config.py
+++ config/jupyterhub_config.py
@@ -1,5 +1,5 @@
 import os
-os.environ['JUPYTER_CONFIG_DIR'] = '/usr/local/share/jupyter'
+os.environ['JUPYTER_CONFIG_DIR'] = '/etc/jupyter'
 c.Spawner.env_keep.append('JUPYTER_CONFIG_DIR')
 c.Spawner.env_keep.append('HADOOP_HOME')
 c.Spawner.notebook_dir = '~/notebooks'
